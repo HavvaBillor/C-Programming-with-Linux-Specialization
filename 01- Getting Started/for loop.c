8cc8a5513c63bf25417b9aa62719c352cf959f6a
Havva Billor <havvabillorx@gmail.com>
2023-07-10 14:50:49 +0300

    for loop

---
+++ 01- Getting Started/for loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+	int i;
+	for(i=0; i<3; i++){
+		printf("C is fun!\n");
+	}
+
+printf("\n");
+    for(i=3; i<8; i++){
+    	printf("We can do everything!!!\n");
+    }
+
+    return 0;
+}
